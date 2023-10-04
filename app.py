d86d339e4e9c80520c6e1c39e682f1fa62876faf
jukebox101 <leonora.squires@gmail.com>
2023-10-04 15:52:33 -0400

    user model updated with user auth and password encryption

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, flash, render_template, redirect, request, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_required, current_user
-import os
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import bcrypt
 # from models import TriviaSet, Question
 
 app = Flask(__name__)
@@ -9,10 +9,80 @@ app.config['SECRET_KEY'] = 'Ctrl-Alt-Defeat-200'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///multinerd.db'
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    from models import User
+    return User.get(user_id)
 
 @app.route('/')
 def index():
     return "Welcome to the Trivia App"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    from models import User
+
+    if request.method == 'POST':
+        # Get user registration data from the form
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if a user with the same username or email already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose a different one.', 'danger')
+        else:
+            existing_email = User.query.filter_by(email=email).first()
+            if existing_email:
+                flash('Email address already registered.', 'danger')
+            else:
+                # Create a new User instance with the hashed password
+                new_user = User(username=username, email=email, password=password)
+
+                try:
+                    # Add the new user to the database
+                    db.session.add(new_user)
+                    db.session.commit()
+                    flash('Registration successful. You can now log in.', 'success')
+                    return redirect(url_for('login'))
+                except Exception as e:
+                    db.session.rollback()
+                    flash('An error occurred during registration. Please try again.', 'danger')
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    from models import User
+    if request.method == 'POST':
+        # Check the user's credentials (e.g., username and password)
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and user.check_password(request.form['password']):
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    # Only authenticated users can access this route
+    return render_template('dashboard.html', user=current_user)
 
 # Route to list all Trivia Sets
 @app.route('/trivia_sets')
@@ -85,6 +155,7 @@ def play_trivia_set(set_id):
     return render_template('play_set.html', trivia_set=trivia_set, questions=questions)
 
 if __name__ == '__main__':
+
 
 
     # database_file = 'test.db'  # Replace with your database file name
